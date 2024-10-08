48710fc7326c6c6959015fd272411481b30e0932
“Matias <124017375+maticorvalan@users.noreply.github.com>
2024-10-08 17:42:24 -0300

    agregado de carrousel y demas cosas

---
+++ script.js
@@ -0,0 +1,29 @@
+let cont = 0;
+let imagenes = ["images/carrouse1.jpg",
+                "images/carrouse2.jpg",
+                "images/carrouse3.jpg",
+                "images/carrouse4.jpg",
+                "images/carrouse5.jpg",
+                "images/carrouse6.jpg",
+                "images/carrouse7.jpg",
+                "images/carrouse8.jpg",
+                ];
+document.imagenCarrousel.src = imagenes[0];
+let btnAdelante = document.querySelector(".adelante");
+let btnAtras = document.querySelector(".atras");
+function forward(){
+    cont++;
+    if(cont > imagenes.length -1){
+        cont=0;
+    }
+    document.imagenCarrousel.src = imagenes[cont];
+}
+btnAdelante.addEventListener("click",forward);
+function back(){
+    cont--;
+    if(cont < 0){
+        cont = imagenes.length -1;
+    }
+    document.imagenCarrousel.src = imagenes[cont];
+}
+btnAtras.addEventListener("click",back);
